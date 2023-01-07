a81a017fe3bb82af1613d33d20d80acf80d889c5
UserKotowaty <barkotzapasowy@gmail.com>
2023-01-07 15:30:43 +0100

    Queen capturing through her team pawn bug fix

--- src/main/java/com/checkers/bartolini/checkersModel/QueenPawnPossibleMovesChecker.java
+++ src/main/java/com/checkers/bartolini/checkersModel/QueenPawnPossibleMovesChecker.java
@@ -16,22 +16,22 @@ public class QueenPawnPossibleMovesChecker extends PossibleMovesChecker {
         for (int i = 0; i < Math.min(row, column); i++) {
             possibleMove = getLeftUpperField(board, row - i, column - i);
             if (possibleMove == null || possibleMove.containsPawn()) break;
-            toReturn.add(new Move(board.getFieldAtPos(row,column),possibleMove,null));
+            toReturn.add(new Move(board.getFieldAtPos(row, column), possibleMove, null));
         }
         for (int i = 0; i < Math.min(row, board.getWidth() - column - 1); i++) {
             possibleMove = getRightUpperField(board, row - i, column + i);
             if (possibleMove == null || possibleMove.containsPawn()) break;
-            toReturn.add(new Move(board.getFieldAtPos(row,column),possibleMove,null));
+            toReturn.add(new Move(board.getFieldAtPos(row, column), possibleMove, null));
         }
         for (int i = 0; i < Math.min(board.getHeight() - row - 1, column); i++) {
             possibleMove = getLeftLowerField(board, row + i, column - i);
             if (possibleMove == null || possibleMove.containsPawn()) break;
-            toReturn.add(new Move(board.getFieldAtPos(row,column),possibleMove,null));
+            toReturn.add(new Move(board.getFieldAtPos(row, column), possibleMove, null));
         }
         for (int i = 0; i < Math.min(board.getHeight() - row - 1, board.getWidth() - column - 1); i++) {
             possibleMove = getRightLowerField(board, row + i, column + i);
             if (possibleMove == null || possibleMove.containsPawn()) break;
-            toReturn.add(new Move(board.getFieldAtPos(row,column),possibleMove,null));
+            toReturn.add(new Move(board.getFieldAtPos(row, column), possibleMove, null));
         }
         return toReturn;
     }
@@ -48,10 +48,12 @@ public class QueenPawnPossibleMovesChecker extends PossibleMovesChecker {
         for (int i = 0; i < Math.min(row, col); i++) {
             possibleMove = getLeftUpperField(board, row - i, col - i);
             if (possibleMove == null) break;
-            if (possibleMove.containsPawn() && possibleMove.getPawnOnField().getTeam()!=team) {
-                possiblePositionAfterTake = getLeftUpperField(board, possibleMove.getRow(), possibleMove.getColumn());
-                if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
-                    toReturn.add(new Move(board.getFieldAtPos(row,col),possiblePositionAfterTake,possibleMove));
+            if (possibleMove.containsPawn()) {
+                if (possibleMove.getPawnOnField().getTeam() != team) {
+                    possiblePositionAfterTake = getLeftUpperField(board, possibleMove.getRow(), possibleMove.getColumn());
+                    if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
+                        toReturn.add(new Move(board.getFieldAtPos(row, col), possiblePositionAfterTake, possibleMove));
+                    }
                 }
                 break;
             }
@@ -59,10 +61,12 @@ public class QueenPawnPossibleMovesChecker extends PossibleMovesChecker {
         for (int i = 0; i < Math.min(row, board.getWidth() - col - 1); i++) {
             possibleMove = getRightUpperField(board, row - i, col + i);
             if (possibleMove == null) break;
-            if (possibleMove.containsPawn() && possibleMove.getPawnOnField().getTeam()!=team) {
-                possiblePositionAfterTake = getRightUpperField(board, possibleMove.getRow(), possibleMove.getColumn());
-                if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
-                    toReturn.add(new Move(board.getFieldAtPos(row,col),possiblePositionAfterTake,possibleMove));
+            if (possibleMove.containsPawn()) {
+                if (possibleMove.getPawnOnField().getTeam() != team) {
+                    possiblePositionAfterTake = getRightUpperField(board, possibleMove.getRow(), possibleMove.getColumn());
+                    if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
+                        toReturn.add(new Move(board.getFieldAtPos(row, col), possiblePositionAfterTake, possibleMove));
+                    }
                 }
                 break;
             }
@@ -70,10 +74,12 @@ public class QueenPawnPossibleMovesChecker extends PossibleMovesChecker {
         for (int i = 0; i < Math.min(board.getHeight() - row - 1, col); i++) {
             possibleMove = getLeftLowerField(board, row + i, col - i);
             if (possibleMove == null) break;
-            if (possibleMove.containsPawn() && possibleMove.getPawnOnField().getTeam()!=team) {
-                possiblePositionAfterTake = getLeftLowerField(board, possibleMove.getRow(), possibleMove.getColumn());
-                if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
-                    toReturn.add(new Move(board.getFieldAtPos(row,col),possiblePositionAfterTake,possibleMove));
+            if (possibleMove.containsPawn()) {
+                if (possibleMove.getPawnOnField().getTeam() != team) {
+                    possiblePositionAfterTake = getLeftLowerField(board, possibleMove.getRow(), possibleMove.getColumn());
+                    if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
+                        toReturn.add(new Move(board.getFieldAtPos(row, col), possiblePositionAfterTake, possibleMove));
+                    }
                 }
                 break;
             }
@@ -81,10 +87,12 @@ public class QueenPawnPossibleMovesChecker extends PossibleMovesChecker {
         for (int i = 0; i < Math.min(board.getHeight() - row - 1, board.getWidth() - col - 1); i++) {
             possibleMove = getRightLowerField(board, row + i, col + i);
             if (possibleMove == null) break;
-            if (possibleMove.containsPawn() && possibleMove.getPawnOnField().getTeam()!=team) {
-                possiblePositionAfterTake = getRightLowerField(board, possibleMove.getRow(), possibleMove.getColumn());
-                if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
-                    toReturn.add(new Move(board.getFieldAtPos(row,col),possiblePositionAfterTake,possibleMove));
+            if (possibleMove.containsPawn()) {
+                if (possibleMove.getPawnOnField().getTeam() != team) {
+                    possiblePositionAfterTake = getRightLowerField(board, possibleMove.getRow(), possibleMove.getColumn());
+                    if (possiblePositionAfterTake != null && !possiblePositionAfterTake.containsPawn()) {
+                        toReturn.add(new Move(board.getFieldAtPos(row, col), possiblePositionAfterTake, possibleMove));
+                    }
                 }
                 break;
             }
